63a0cad686cd365850d6abfe8546e79834cd6fa4
mafei <535977672@qq.com>
2019-05-26 17:12:17 +0800

    6

--- application/index/controller/Ffmpegs.php
+++ application/index/controller/Ffmpegs.php
@@ -1,17 +1,7 @@
 <?php
 namespace app\index\controller;
-        
-require '../vendor/autoload.php';
 
 use think\Controller;
-
-use FFMpeg\FFMpeg;
-use FFMpeg\FFProbe;
-use FFMpeg\Coordinate\Dimension;
-use FFMpeg\Coordinate\TimeCode;
-use FFMpeg\Format\Video\X264;
-use FFMpeg\Format\Video\WMV;
-use FFMpeg\Filters\Video\ExtractMultipleFramesFilter;
 
 set_time_limit(0);
 
@@ -34,45 +24,77 @@ ini_set('memory_limit', '-1');
  * ffmpeg [全局选项] {[输入文件选项] -i 输入文件} ... {[输出文件选项] 输出文件} ...即
  * ffmpeg [global_options] {[input_file_options] -i input_file} ... {[output_file_options] output_file} ...
  * 
-    -i 设定输入流
-    -f 设定输出格式
-    -ss 开始时间 
-    视频参数： 
-    -b 设定视频流量，默认为200Kbit/s 
-    -r 设定帧速率，默认为25 
-    -s 设定画面的宽与高 
-    -aspect 设定画面的比例 
-    -vn 不处理视频 
-    -vcodec 设定视频编解码器，未设定时则使用与输入流相同的编解码器 
-    音频参数： 
-    -ar 设定采样率 
-    -ac 设定声音的Channel数 
-    -acodec 设定声音编解码器，未设定时则使用与输入流相同的编解码器 
-    -an 不处理音频
-
-    -ss 00:00:06 -t 00:00:12 6秒开始，时长为12秒
-    -vcodec copy -acodec copy : 编码格式不变
-    -y 对输出文件进行覆盖
-    -threads 2 以两个线程进行运行 加快处理的速度
-    -vcode 编码格式libx264 
-    -f 强迫采用格式fmt 
-    -q:v 2 视频码率 提高抽取到的图片的质量的  
+ *  -i 设定输入流
+ *  -f 设定输出格式
+ *  -ss 开始时间 
+ *  视频参数： 
+ *  -b 设定视频流量，默认为200Kbit/s 
+ *  -r 设定帧速率，默认为25 
+ *  -s 设定画面的宽与高 
+ *  -aspect 设定画面的比例 
+ *  -vn 不处理视频 
+ *  -vcodec 设定视频编解码器，未设定时则使用与输入流相同的编解码器 
+ *  音频参数： 
+ *   -ar 设定采样率 
+ *  -ac 设定声音的Channel数 
+ *  -acodec 设定声音编解码器，未设定时则使用与输入流相同的编解码器 
+ *  -an 不处理音频
+ *  
+ *   -ss 00:00:06 -t 00:00:12 6秒开始，时长为12秒
+ *  -vcodec copy -acodec copy : 编码格式不变
+ *  -y 对输出文件进行覆盖
+ *  -threads 2 以两个线程进行运行 加快处理的速度
+ *  -vcode 编码格式libx264 
+ *  -f 强迫采用格式fmt 
+ *  -q:v 2 视频码率 提高抽取到的图片的质量的  
+ * 
+ * 读取文件有两种方式:一种是直接读取,文件被迅速读完;一种是按时间戳读取。一般都是按时间戳读取文件
+ * -re -i，表示按时间戳读取文件
+ * -shortest 当最短的输入流结束后即停止编码和输出 shortest=1就是当输入停止的时候延迟1秒结束
+ * nullsrc=s=256x256 nullsrc作为视频源，宽高为256x256， 默认绿屏 
+ * '-vf "delogo=x=15:y=5:w=320:h=120:show=1" 去logo,show=1,显示确认框确认坐标
+ * 
+ * 
+ *  选项
+ *  - -y / -n
+ *  - -codec(-c)
+ *  - -ss
+ *  - -t
+ *   - -to
+ *  - -f
+ *  - -filter / -filter_complex
+ *  - -vframes
+ *  - -vn
+ *  - -r
+ *  - -s
+ *  - -an
+ *  - -threads
+ *  - -shortest
+ *      filter
+ *      scale
+ *      crop
+ *      overlay
+ *      drawtext
+ *      fade
+ *      fps
  */
 class Ffmpegs extends Controller
 {
     protected $ffmpeg = '';
     protected $ffprobe = '';
     protected $is_linux = false;
+    protected $debug = false;
 
     protected function initialize() {
         if(strtoupper(substr(PHP_OS,0,3))!=='WIN'){
-            $this->ffmpeg = 'ffmpeg';
-            $this->ffprobe = 'ffprobe';      
+            $this->ffmpeg = 'ffmpeg -threads 8';
+            $this->ffprobe = 'ffprobe -threads 8';      
             $this->is_linux = true;
         }else{
-            $this->ffmpeg = 'D:/ffmpeg/bin/ffmpeg.exe';
-            $this->ffprobe = 'D:/ffmpeg/bin/ffprobe.exe';
+            $this->ffmpeg = 'D:/ffmpeg/bin/ffmpeg.exe -threads 8';
+            $this->ffprobe = 'D:/ffmpeg/bin/ffprobe.exe -threads 8';
             $this->is_linux = false;
+            $this->debug = true;
         }
         
         //overlay
@@ -105,14 +127,95 @@ class Ffmpegs extends Controller
 //        ];
 //        $this->concat($source, $durl2);
         
-        $durl2 = 'static/ffmpeg/1_1_2.mp4';
-        $source = [
-            'static/ffmpeg/1.mp4',
-            'static/ffmpeg/2.mp4'
-        ];
-        //$this->overlay($source, $durl2);
+//        $durl2 = 'static/ffmpeg/1_1_2.mp4';
+//        $source = [
+//            'static/ffmpeg/1.mp4',
+//            'static/ffmpeg/2.mp4'
+//        ];
+//        $this->overlay($source, $durl2, 0, 50, 50);
+        
+//        $source =  'static/ffmpeg/1.mp4';
+//        $file = 'static/ffmpeg/1_logo.mp4';
+//        $file2 = 'static/ffmpeg/1_logo2.mp4';
+//        $info = $this->videoInfo($source);
+//        $this->logo($source, $file, 15, 15, 330, 120);
+//        $this->logo($file, $file2, $info[0]['width']-345, $info[0]['height']-135, 330, 120);
+        
+//        $source =  'static/ffmpeg/1.mp4';
+//        $file = 'static/ffmpeg/1_copy.mp4';
+//        $info = $this->videoInfo($source);
+//        $this->copy($source, $file, 3, 5);
         
         
+        
+    }
+    
+    public function test() {
+        $source =  'static/ffmpeg/1.mp4';
+        $file = 'static/ffmpeg/1_logo.mp4';
+        $file2 = 'static/ffmpeg/1_logo2.mp4';
+        $info = $this->videoInfo($source);
+        $this->logo($source, $file, 15, 15, 330, 120);
+        $this->logo($file, $file2, $info[0]['width']-345, $info[0]['height']-135, 330, 120);
+        
+        $file3 = 'static/ffmpeg/1_overlay4_1.mp4';
+        $info = $this->overlay4($file2,$file3);
+        
+        $file4 = 'static/ffmpeg/1_overlay4_2.mp4';
+        $info = $this->overlay2([$file3,$file2,$file2],$file4);
+        
+        
+    }
+    
+    public function overlay2($sou, $file) {
+        //$source =  'static/ffmpeg/1.mp4';
+        //$file = 'static/ffmpeg/1_9_overlay2.mp4';
+        
+        if(is_array($sou)){
+            $source1 = $sou[1];
+            $source2 = $sou[2];
+            $source = $sou[0];
+        }else{
+            $source1 = $source2 = $source = $sou;
+        }
+        
+        $info = $this->videoInfo($source);
+        $w = $info[0]['width'];
+        $h = $info[0]['height'];
+        $w2 = $w/2;
+        $h2 = $h/2;
+        $h4 = $h/4;
+        
+        $str = $this->ffmpeg . " -y -re -i $source -re -i $source1 -re -i $source2 " 
+            . '-filter_complex "[1:v] setpts=PTS-STARTPTS, scale='. $w2 .'x'. $h2 .' [left]; [2:v] setpts=PTS-STARTPTS, scale='. $w2 .'x'. $h2 .' [right]; [0:v] [left] overlay=shortest=1:x=0:y='. $h4 .' [tmp1]; [tmp1][right] overlay=shortest=1:x='. $w2 .':y='. $h4 .'" '
+            . $file;
+        
+        $this->execDebug($str);
+    }
+    
+    public function overlay4($source, $file) {
+        //$source =  'static/ffmpeg/1.mp4';
+        //$file = 'static/ffmpeg/1_9_overlay4.mp4';
+        
+        $info = $this->videoInfo($source);
+        $w = $info[0]['width'];
+        $h = $info[0]['height'];
+        $w2 = $w/2;
+        $h2 = $h/2;
+        
+        $str = $this->ffmpeg . " -y -re -i $source -re -i $source  -re -i $source  -re -i $source " 
+        . '-filter_complex "nullsrc=size='. $w .'x'. $h .' [base];'
+        . '[0:v] setpts=PTS-STARTPTS, scale='. $w2 .'x'. $h2 .' [upperleft]; '
+        . '[1:v] setpts=PTS-STARTPTS, scale='. $w2 .'x'. $h2 .' [upperright]; '
+        . '[2:v] setpts=PTS-STARTPTS, scale='. $w2 .'x'. $h2 .' [lowerleft]; '
+        . '[3:v] setpts=PTS-STARTPTS, scale='. $w2 .'x'. $h2 .' [lowerright]; '
+        . '[base] [upperleft] overlay=shortest=1:x=0:y=0 [tmp1]; '
+        . '[tmp1] [upperright] overlay=shortest=1:x='. $w2 .':y=0 [tmp2]; '
+        . '[tmp2] [lowerleft] overlay=shortest=1:x=0:y=' . $h2 .' [tmp3]; '
+        . '[tmp3] [lowerright] overlay=shortest=1:x='. $w2 .':y='. $h2 .' " '
+        . $file;
+        
+       $this->execDebug($str);
     }
     
     /**
@@ -164,6 +267,7 @@ class Ffmpegs extends Controller
         $ret = array();
         
         $video_info = preg_replace('/\([^\(, ]*?, [^\), ]*?\)/', '', $video_info);
+        $video_info = preg_replace('/\[(.*?)\]/', '', $video_info);
 
         // Duration: 00:33:42.64, start: 0.000000, bitrate: 152 kb/s
         if (preg_match("/Duration: (.*?), start: (.*?), bitrate: (\d*) kb\/s/", $video_info, $matches)){
@@ -176,14 +280,15 @@ class Ffmpegs extends Controller
 
         // Stream #0:1: Video: rv20 (RV20 / 0x30325652), yuv420p, 352x288, 117 kb/s, 15 fps, 15 tbr, 1k tbn, 1k tbc
         // Stream #0:0(und): Video: h264 (Baseline) (avc1 / 0x31637661), yuv420p(tv, smpte170m/bt470bg/smpte170m), 720x1280, 5972 kb/s, 9.96 fps, 20.58 tbr, 1000k tbn, 2000k tbc (default)
+        // Stream #0:0(und): Video: h264 (High) (avc1 / 0x31637661), yuv420p, 720x1280 [SAR 1:1 DAR 9:16], 1390 kb/s, 25 fps, 25 tbr, 12800 tbn, 50 tbc (default)
         if(preg_match("/Video: (.*?), (.*?), (.*?), (.*?), (.*?) fps[,\s]/", $video_info, $matches)){
             $ret['vcodec'] = $matches[1];     // 编码格式
             $ret['vformat'] = $matches[2];    // 视频格式
             $ret['resolution'] = $matches[3]; // 分辨率
             $ret['fps'] = $matches[5]; // 帧
             list($width, $height) = explode('x', $matches[3]);
-            $ret['width'] = $width;
-            $ret['height'] = $height;
+            $ret['width'] = floatval($width);
+            $ret['height'] = floatval($height);
         }
 
         // Stream #0:0: Audio: cook (cook / 0x6B6F6F63), 22050 Hz, stereo, fltp, 32 kb/s
@@ -211,24 +316,8 @@ class Ffmpegs extends Controller
 
         $this->createDir($durl);
         $str = "$this->ffmpeg -i $url -y -f image2 -ss $ss -t $t -s " . $w . "x" . $h . " $durl";
-        exec($str);
+        $this->execDebug($str);
     }
-    
-    public function videoMerge(){
-        
-        $row = 1;//1-水平 0-垂直
-        $url = [
-            'static/ffmpeg/1.mp4',
-            'static/ffmpeg/2.mp4'
-        ];
-        $len = count($url);
-        
-        $ffmpeg = $this->ffmpeg;
-        
-        $str = "$this->ffmpeg -i $url image%d.jpg";
-        
-    }
-    
     
     /**
      * 视频转图片
@@ -247,7 +336,7 @@ class Ffmpegs extends Controller
         
         $str = "$this->ffmpeg -i $url -r $r $durl";
         
-        exec($str);
+        $this->execDebug($str);
         $imgName = glob($d);
         return $imgName?$imgName:[];
         
@@ -268,7 +357,7 @@ class Ffmpegs extends Controller
         
         $str = "$this->ffmpeg -y -r $r -f image2 -i $url -vcodec libx264 $durl";
         
-        exec($str);
+        $this->execDebug($str);
         
         $d = $this->getDir($url);
         $this->removeDir($d);
@@ -291,7 +380,7 @@ class Ffmpegs extends Controller
         //mp4转成无损质量的ts
         $str = "$this->ffmpeg -i $url -vcodec copy -acodec copy -vbsf h264_mp4toannexb $durl";
         
-        exec($str);
+        $this->execDebug($str);
  
         return true; 
     }
@@ -307,12 +396,12 @@ class Ffmpegs extends Controller
         foreach ($source as $k=>$v) {
             $tempFileName = "$dir/1_$k.ts";
             $str = "$this->ffmpeg -i $v -vcodec copy -acodec copy -vbsf h264_mp4toannexb $tempFileName";
-            exec($str);
+            $this->execDebug($str);
             $ts .= '|'.$tempFileName;
         }
         $ts = substr($ts, 1);
         $str = $this->ffmpeg . ' -y -i "concat:' . $ts . '" -acodec copy -vcodec copy -absf aac_adtstoasc ' . $file;
-        exec($str);
+        $this->execDebug($str);
         $this->removeDir($dir);
         //ffmpeg -i "concat:1.ts|2.ts" -acodec copy -vcodec copy -absf aac_adtstoasc output.mp4
 
@@ -344,7 +433,34 @@ class Ffmpegs extends Controller
             $str .= " -itsoffset $itsoffset";
         }
         $str .= " -filter_complex overlay=$x:$y $file";
-        exec($str);
+        $this->execDebug($str);
+        
+    }
+    
+    /**
+     * 去logo
+     * '-vf "delogo=x=15:y=5:w=320:h=120:show=1" 去logo,show=1,显示确认框确认坐标
+     */
+    public function logo($source, $file, $x = 0, $y = 0, $w = 0, $h = 0) {
+        $this->createDir($file);
+        $str = $this->ffmpeg . " -y -i $source " 
+            . '-vf "delogo=x='. $x .':y='. $y .':w='. $w .':h='. $h .'" '
+            . $file;
+        $this->execDebug($str);
+        
+    }
+    
+    /**
+     * 剪切
+     * ffmpeg -ss 00:00:01 -t 00:00:10 -i input.mp4 -vcodec copy -acodec copy output.mp4
+     * -ss开始时间 -ss要在-i之前
+     * -t 时长
+     * -vcodec copy -acodec copy 编码格式不变
+     */
+    public function copy($source, $file, $ss = '00:00:01', $t = '00:00:01') {
+        $this->createDir($file);
+        $str = $this->ffmpeg . " -y  -ss $ss -t $t -i $source -vcodec copy -acodec copy $file";
+        $this->execDebug($str);
         
     }
 
@@ -386,6 +502,20 @@ class Ffmpegs extends Controller
     
     public function randName() {
         return md5(uniqid(rand()));
+    }
+    
+    public function execDebug($str){
+        if($this->debug){
+            ob_start();
+            $str = $str . ' 2>&1';
+            //执行外部程序并且显示原始输出
+            passthru($str);
+            $re = ob_get_contents();
+            ob_end_clean();
+            dump($re);
+        }else{
+            exec($str);
+        }
     }
 }
 
